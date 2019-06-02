303569376c10bdd85dad64a4440f1775be4a1f0d
AnshumaanSV <anshumaansv@gmail.com>
2019-06-02 16:33:18 +0530

    initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,113 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Controller {
+    static String path;
+
+    @FXML
+    private VBox mainContainer;
+    private List<Button> movieButtons;
+
+    public void openSpDialogue(ActionEvent a){
+        Stage selectPathStage = new Stage();
+        selectPathStage.initModality(Modality.APPLICATION_MODAL);
+        selectPathStage.setTitle("Select Path");
+        selectPathStage.setMinWidth(400);
+        selectPathStage.setMinHeight(60);
+
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+        configuringDirectoryChooser(directoryChooser);
+
+        TextField pathTextField = new TextField();
+        Button button = new Button("Select a directory");
+
+        button.setOnAction(event -> { File dir = directoryChooser.showDialog(selectPathStage);
+
+            boolean update = false;
+
+            if (dir != null) {
+                pathTextField.setText(dir.getAbsolutePath());
+                path = pathTextField.getText();
+                update = true;
+            } else {
+                pathTextField.setText(null);
+            }
+            selectPathStage.close();
+
+            if(update) {
+                getMovies();
+            }
+        });
+
+        VBox root = new VBox();
+        root.setPadding(new Insets(10));
+        root.setSpacing(5);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(pathTextField, button);
+
+        Scene scene = new Scene(root, 400, 60);
+
+        selectPathStage.setTitle("Choose Directory");
+        selectPathStage.setScene(scene);
+        selectPathStage.show();
+    }
+
+    public void getMovies() {
+        movieButtons = new ArrayList<>();
+        Path chooseFromPath = Paths.get(path);
+        try (Stream<Path> subPaths = Files.walk(chooseFromPath)) {
+            subPaths.forEach(a -> {
+                if(a.toString().contains(".mp4") || a.toString().contains(".avi") ||
+                   a.toString().contains(".flv") || a.toString().contains(".mkv") ||
+                   a.toString().contains(".webm") || a.toString().contains(".wav")) {
+                    Button button = new Button(a.toString());
+                    button.setStyle("-fx-background-color: #C468F4; ");
+                    movieButtons.add(button);
+                    mainContainer.getChildren().add(button);
+                }
+            });
+        } catch (IOException E) {}
+    }
+
+    private static void configuringDirectoryChooser(DirectoryChooser directoryChooser) {
+        directoryChooser.setTitle("Select Directory");
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+    }
+
+    public void openFbDialogue(ActionEvent a) throws IOException {
+        Stage fbStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Pane root = fxmlLoader.load(getClass().getResource("feedbackLinks.fxml").openStream());
+        fbStage.setScene(new Scene(root,364,174));
+        fbStage.showAndWait();
+    }
+
+    public void openAbtDialogue(ActionEvent a) throws IOException {
+        Stage abtStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Pane root = fxmlLoader.load(getClass().getResource("aboutProject.fxml").openStream());
+        abtStage.setScene(new Scene(root, 700, 300));
+        abtStage.showAndWait();
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.awt.*;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("ILAP Player");
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+
+        primaryStage.setScene(new Scene(root, width, height));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
